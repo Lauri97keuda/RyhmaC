73206cba3a25959336cda88e87dd82ec8c1d0a57
Lauri97keuda <162415465+Lauri97keuda@users.noreply.github.com>
2024-04-17 10:55:54 +0300

    keskiviikon sotkut

---
+++ SaraStrong/Sara strong.js
@@ -0,0 +1,12 @@
+//yläpainikkeet//
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
+//reseptit//
+
