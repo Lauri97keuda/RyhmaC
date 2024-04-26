83948d5187ffe5fd6e182f76a9b51800d0ae818c
SAVOULIAK <164487500+SAVOULIAK@users.noreply.github.com>
2024-04-26 11:15:35 +0300

    blogi
    changed the design,
    added a blog section

--- SaraStrong/Sara strong.js
+++ SaraStrong/Sara strong.js
@@ -1,17 +1,24 @@
+// Файл: Sara strong.js
+
 //yläpainikkeet näytä ja piilota sivupalkki//
 
 function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display ='flex'
-  }
-  function hideSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display ='none'
-  }
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display ='flex'
+}
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display ='none'
+}
 
-  //reseptit//
-  
- //kalenteri//
+//reseptit//
 
- //blogi//
+//kalenteri//
 
+//blogi//
+
+// Добавляем новую функцию openAdditionalText
+function openAdditionalText() {
+  // Ваш код здесь
+  console.log("Функция openAdditionalText была вызвана");
+}
