dc76326141b5753a23789a0c316e268675521e03
Mohnatish <156414913+Mohnatish@users.noreply.github.com>
2024-05-13 14:14:23 +0300

    Blog
    Modal_Windows script muokattu - nyt jos avattu 1 ikkuna ja avat uuden 1 mene kiinni, muokattu sulkemis rasti - fontti ja sijainti, muutettu Boxijen tausta väri, muutettu Modal ikkunoiden taustaväri.

--- SaraStrong/Modal_Windows.js
+++ SaraStrong/Modal_Windows.js
@@ -1,17 +1,24 @@
 //blogi//
-//Modal ikkunan Avaus//
+//Modal ikkunat
 function openModal(modalId) {
+    // Kaikkijen Modal ikkunoiden kiinnitys jos avat uuttaa Modal ikkuna
+    var allModals = document.getElementsByClassName("modal");
+    for (var i = 0; i < allModals.length; i++) {
+        allModals[i].style.display = "none";
+    }
+
+    // Valittun Modal ikkunoiden avaus
     var modal = document.getElementById(modalId);
     modal.style.display = "block";
 }
 
-// Modal Ikkunan Kiinitys
+// Modal ikkunan kiinnitys
 function closeModal(modalId) {
     var modal = document.getElementById(modalId);
     modal.style.display = "none";
 }
 
-// Modal Ikkunan Kiinitys jos painat jossain vieren
+// Modal ikkunoiden kiinnitys jos painat jossain mualla
 window.onclick = function(event) {
     var modals = document.getElementsByClassName("modal");
     for (var i = 0; i < modals.length; i++) {
